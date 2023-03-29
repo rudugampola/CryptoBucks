2c17a0ab4f134540f17c148f48d3698a7e607a4f
rudugampola <ravindu365@gmail.com>
2023-03-28 22:26:36 -0700

    first commit

---
+++ src/components/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoSvg from '../assets/logo.svg';
+
+const logo = () => {
+  return (
+    <Link
+      to="/"
+      className="absolute top-[1.5rem] left-[1.5rem] [text-decoration:none] text-lg text-cyan flex items-center"
+    >
+      <img src={logoSvg} alt="CryptoBucks" />
+      <span>CryptoBucks</span>
+    </Link>
+  );
+};
+
+export default logo;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+  return (
+    <main className="w-full h-full flex flex-col first-letter: content-cent items-center relative text-white font-nunito">
+      <div className="w-screen h-screen bg-gray-300 fixed -z-10"></div>
+      <Logo />
+      <Navigation />
+      <Outlet />
+    </main>
+  );
+};
+
+export default Home;
