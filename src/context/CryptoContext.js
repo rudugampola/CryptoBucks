d695e9fd81816031e9dae4dcd145d5f938fec88f
rudugampola <ravindu365@gmail.com>
2023-03-30 22:16:19 -0700

    Search and sorting implemented

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -5,13 +5,19 @@ export const CryptoContext = createContext({});
 
 // Create a provider for components to consume and subscribe to changes
 export const CryptoProvider = ({ children }) => {
-  const [cryptoData, setCryptoData] = useState('this is test state');
+  const [cryptoData, setCryptoData] = useState();
+  const [searchData, setSearchData] = useState();
+  const [coinSearch, setCoinSearch] = useState('');
 
-  const getCryptoData = async () => {
+  const [currency, setCurrency] = useState('USD');
+
+  const getCoinData = async () => {
     try {
-      const data =
-        await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en
-      `).then(((res) => res.json()).then((json) => json));
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&ids=${coinSearch}&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
+      )
+        .then((res) => res.json())
+        .then((json) => json);
 
       console.log(data);
       setCryptoData(data);
@@ -20,12 +26,52 @@ export const CryptoProvider = ({ children }) => {
     }
   };
 
+  const getCryptoData = async () => {
+    try {
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
+      )
+        .then((res) => res.json())
+        .then((json) => json);
+
+      console.log(data);
+      setCryptoData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getSearchResult = async (query) => {
+    try {
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/search?query=${query}`
+      )
+        .then((res) => res.json())
+        .then((json) => json);
+
+      console.log(data);
+      setSearchData(data.coins);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useLayoutEffect(() => {
     getCryptoData();
-  }, []);
+  }, [coinSearch, currency]);
 
   return (
-    <CryptoContext.Provider value={{ cryptoData }}>
+    <CryptoContext.Provider
+      value={{
+        cryptoData,
+        searchData,
+        getSearchResult,
+        setCoinSearch,
+        setSearchData,
+        currency,
+        setCurrency,
+      }}
+    >
       {children}
     </CryptoContext.Provider>
   );
--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Filters from '../components/Filters';
 import TableComponent from '../components/TableComponent';
 
 const Crypto = () => {
   return (
     <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+      <Filters />
       <TableComponent />
     </section>
   );
